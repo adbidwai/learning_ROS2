9afabdda9ea64be069c2cc2b9e1412ba8ad286ae
Aditya Bidwai <46625413+adbidwai@users.noreply.github.com>
2020-09-10 00:08:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 sudo apt update && sudo apt install curl gnupg2 lsb-release wget
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
-wget https://github.com/ros2/ros2/releases/download/release-foxy-20200807/ros2-foxy-20200807-linux-focal-amd64.tar.bz2
+wget https://github.com/ros2/ros2/releases/download/release-foxy-20200807/ros2-foxy-20200807-linux-focal-amd64.tar.bz2 -P ~/Downloads/
 mkdir -p ~/ros2_foxy
 cd ~/ros2_foxy
 tar xf ~/Downloads/ros2-foxy-linux-x86_64.tar.bz2
